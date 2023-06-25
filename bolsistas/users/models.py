cfbe04395bddaf3e6edaffd5f05a9f86ca95c34d
Ferdinandy <ferdinandsc@yahoo.com.br>
2023-06-25 09:08:39 -0300

    new app

--- bolsistas/users/models.py
+++ bolsistas/users/models.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
+
+from frequencias.models import Campus, Projeto
 
 
 class User(AbstractUser):
@@ -12,7 +14,7 @@ class User(AbstractUser):
     """
 
     #: First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore
     last_name = None  # type: ignore
 
@@ -24,3 +26,22 @@ class User(AbstractUser):
 
         """
         return reverse("users:detail", kwargs={"username": self.username})
+
+class ProReitor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    inicio_m = models.DateField(null=True)
+    fim_m = models.DateField(null=True)
+
+class Coordenador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+
+class Orientador(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    situacao = models.CharField(max_length=150)
+
+class Bolsista(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    orientador = models.ForeignKey(Orientador, on_delete=models.CASCADE, related_name="bolsistas")
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE)
+    projeto = models.ForeignKey(Projeto, on_delete=models.CASCADE)
