de25b98ca301f100cc51af54fbe60b6a40febe5e
Ferdinandy <ferdinandsc@yahoo.com.br>
2023-06-24 23:54:04 -0300

    project reset

---
+++ bolsistas/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bolsistas.users.models import User
+from bolsistas.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
